087afbb387263e12361c8acdb7f115503f6cc98b
Jackie Chan <jackiec1998@gmail.com>
2025-07-15 17:02:03 -0400

    Created a frontend.

---
+++ react/src/App.tsx
@@ -0,0 +1,20 @@
+function App() {
+	return (
+		<div className="flex justify-center">
+			<div className="w-[500px] mt-4">
+				<h1 className="text-2xl font-bold">
+					Trump's Ties to Epstein & Maxwell
+				</h1>
+
+				<article className="my-4">
+					<h2>
+						There are at least 10 documented instances where Trump appeared in
+						photos with Epstein or Maxwell.
+					</h2>
+				</article>
+			</div>
+		</div>
+	);
+}
+
+export default App;
